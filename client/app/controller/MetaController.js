d524b97560a2bb72c9e5caae1a54457635327cd4
George <gnthackray@btinternet.com>
2015-01-24 23:57:08 +0000

    adding meta dialog

---
+++ client/app/controller/MetaController.js
@@ -0,0 +1,37 @@
+var MetaController = function (view, model) {
+    this._view = view;
+
+    this.model = model;
+    
+    this.init();
+    
+    this._view.QryMetaDataState($.proxy(this.qryMetaDataState, this));
+    
+    this._view.QryDataTypeState($.proxy(this.qryDataTypeState, this));
+    
+  
+};
+
+MetaController.prototype = {
+    init:function(){
+    
+         if (this.model !== null) {
+            this.model.GetData();
+         }
+    },
+    
+    qryMetaDataState:function(data){
+        console.log('qryLayerButtonState: '+data);
+      
+             //   this.model.SetCurrent(data.id, data.value);
+          
+    },
+    
+    qryDataTypeState:function(data){
+        console.log('qryLayerButtonState: '+data);
+      
+        this.model.SetDataType(data);
+          
+    }
+    
+};
---
+++ client/app/model/Meta.js
@@ -0,0 +1,51 @@
+var Meta = function (nodestore,view, image) {
+    this.nodestore = nodestore;
+    this.metaData = 1;  
+    this.metaDataTypes = 1;
+    this.view = view;
+    //obviously this needs reworking
+    //but lets get functionality correct first
+    this.image = image;
+    
+};
+
+
+
+Meta.prototype.Save = function(){
+    
+   // this.nodestore.SaveLayers(this.layerData);
+   // this.view.SetLayers(this.layerData);
+};
+
+Meta.prototype.GetMetaDataTypes = function(){
+    
+    
+};
+
+Meta.prototype.GetDataTypes = function(){
+    
+    
+};
+
+Meta.prototype.GetData = function(){
+    
+    var that = this;
+    
+    this.nodestore.GetMetaData()(function(data){
+        that.metaData = data;
+        that.view.SetMetaData(data);
+    });
+    
+    
+
+};
+
+Meta.prototype.SetDataType = function(ids){
+    var that = this;
+    
+    this.nodestore.GetMetaDataTypes()(function(data){
+        that.metaDataTypes = data;
+        that.view.SetMetaDataTypes(data);
+    });
+};
+
