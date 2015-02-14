6e443d88f2bc7efba5536571f31eeea5c04e0f69
George <gnthackray@btinternet.com>
2015-02-14 14:49:19 +0000

    cropper  stuff

--- client/app/imagemain_tester.js
+++ client/app/imagemain_tester.js
@@ -28,36 +28,36 @@ function loadAll (drive){
      
     var noteDataManager = new NoteDataManager(data);
     
-    // noteDataManager.init(function(){
+    noteDataManager.init(function(){
 
-    //     var metadata = new Meta(noteDataManager,appView);
-    //     var metaController = new MetaController(appView,metadata);
+        var metadata = new Meta(noteDataManager,appView);
+        var metaController = new MetaController(appView,metadata);
 
-    //     var options = new Options(noteDataManager,appView);
-    //     var optionsController = new OptionsController(appView,options);
+        var options = new Options(noteDataManager,appView);
+        var optionsController = new OptionsController(appView,options);
 
-    //     var cropper = new Crop(noteDataManager,appView);
-    //     var crapperController = new CroppingController(appView,cropper);
+        // var cropper = new Crop(noteDataManager,appView);
+        // var crapperController = new CroppingController(appView,cropper);
 
 
-    //     var model = new ImageViewer(noteDataManager,appView,  new CanvasTools(), 
-    //                                 metadata, 
-    //                                 options);
+        // var model = new ImageViewer(noteDataManager,appView,  new CanvasTools(), 
+        //                             metadata, 
+        //                             options);
         
         
-    //     var diagramController =  new DiagramController(appView, model);
+        // var diagramController =  new DiagramController(appView, model);
     
     
 
     
-    //     var urls= new Urls(new UrlWriter(),noteDataManager,appView,model.setImageObject);
+        // var urls= new Urls(new UrlWriter(),noteDataManager,appView,model.setImageObject);
         
-    //     var urlController = new UrlController(appView,urls);
+        // var urlController = new UrlController(appView,urls);
         
-    //     var layer = new Layer(noteDataManager,appView, model);
-    //     var layerController = new LayerController(appView,layer);
+        // var layer = new Layer(noteDataManager,appView, model);
+        // var layerController = new LayerController(appView,layer);
 
         
 
-    // });
+    });
 }
