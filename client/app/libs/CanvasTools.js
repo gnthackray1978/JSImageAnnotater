b01c7c455c0e24e2e47d66485494c156bcd5e31a
George <gnthackray@btinternet.com>
2015-02-16 17:24:30 +0000

    cropper  stuff

--- client/app/libs/CanvasTools.js
+++ client/app/libs/CanvasTools.js
@@ -51,8 +51,8 @@ CanvasTools.prototype = {
 
           
             that.ctx.clearRect(0, 0, that.canvas.width, that.canvas.height);
-            
-            if(cropnode.X !=0 && cropnode.Y !=0 && cropnode.LayerId !=4)
+            console.log('DrawCroppedImage: ' +  cropnode.Width + ' ' +cropnode.Height);
+            if(cropnode.Width ==0 && cropnode.Height ==0)
                 that.ctx.drawImage(img, x, y,width,height);
             else
                 that.ctx.drawImage(img, cropnode.X, cropnode.Y,cropnode.Width,cropnode.Height, cropnode.X, cropnode.Y,cropnode.Width,cropnode.Height);
