73aa2ddb4facbe8f8b1756bee712709fd369f32e
George <gnthackray@btinternet.com>
2015-08-30 14:45:59 +0000

    slight refactor of old node code

---
+++ config/serviceStart.js
@@ -0,0 +1,70 @@
+
+var serviceStart = function(){
+    
+    var http           = require('http');
+    var express        = require('express');
+    var morgan         = require('morgan');
+    var bodyParser     = require('body-parser');
+    var methodOverride = require('method-override');
+    var app            = express();
+    var request        = require('request');
+    
+    var port     = process.env.PORT || 8080;
+    var mongoose = require('mongoose');
+    var configDB = require('./database.js');
+    
+    
+    
+    // configuration
+    mongoose.connect(configDB.settings.url, function (err, res) {
+      if (err) {
+    	console.log ('ERROR connecting to: ' + configDB.settings.url + '. ' + err);
+      } else {
+    	console.log ('Succeeded connected to: ' + configDB.settings.url);
+    	 
+    	//console.log(mongoose.connection.db.collection); 
+    	 
+    	mongoose.connection.db.collectionNames(function (err, names) {
+           // console.log(names); // [{ name: 'dbname.myCollection' }]
+    		
+        });
+    
+      }
+    });
+      
+    
+     
+    var oneDay = 1;
+    
+    // set up our express application
+    
+    console.log('static contents at: ' + __dirname + '\\client');
+    app.use(express.static(__dirname + '/client', { maxAge: oneDay }));
+    app.use(morgan('dev')); 											// log every request to the console
+    app.use(bodyParser.json());
+    app.use(methodOverride()); 											// simulate DELETE and PUT 
+     
+     
+    // Make our db accessible to our router
+    
+    app.use(function(req,res,next){
+        console.log('Time: %d', Date.now());
+    	req.NotesModel = mongoose.model('notes', configDB.notes);
+     	req.UrlsModel = mongoose.model('urls', configDB.urls);
+     	req.OptionsModel = mongoose.model('options', configDB.options);
+    	next();
+    });
+    
+    
+    console.log('routing...');
+    
+    // load our routes and pass in our app and fully configured passport
+    require('../app/routes.js')(app,express,request); 
+    
+    // launch
+    app.listen(port);
+    console.log('The magic happens on port ' + port);
+};
+
+
+module.exports.services = new serviceStart();
--- server.js
+++ server.js
@@ -1,71 +1,76 @@
-// server.js
-
-// set up ======================================================================
-// get all the tools we need
-var http     = require('http');
-
-var express        = require('express');
-var morgan         = require('morgan');
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
-var app            = express();
-var request        = require('request');
-
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var configDB = require('./config/database.js');
 
 
+// // server.js
 
-// configuration ===============================================================
-mongoose.connect(configDB.settings.url, function (err, res) {
-  if (err) {
-	console.log ('ERROR connecting to: ' + configDB.settings.url + '. ' + err);
-  } else {
-	console.log ('Succeeded connected to: ' + configDB.settings.url);
+var services = require('./config/serviceStart.js');
+
+
+// // set up ======================================================================
+// // get all the tools we need
+// var http     = require('http');
+
+// var express        = require('express');
+// var morgan         = require('morgan');
+// var bodyParser     = require('body-parser');
+// var methodOverride = require('method-override');
+// var app            = express();
+// var request        = require('request');
+
+// var port     = process.env.PORT || 8080;
+// var mongoose = require('mongoose');
+// var configDB = require('./config/database.js');
+
+
+
+// // configuration ===============================================================
+// mongoose.connect(configDB.settings.url, function (err, res) {
+//   if (err) {
+// 	console.log ('ERROR connecting to: ' + configDB.settings.url + '. ' + err);
+//   } else {
+// 	console.log ('Succeeded connected to: ' + configDB.settings.url);
 	 
-	//console.log(mongoose.connection.db.collection); 
+// 	//console.log(mongoose.connection.db.collection); 
 	 
-	mongoose.connection.db.collectionNames(function (err, names) {
-       // console.log(names); // [{ name: 'dbname.myCollection' }]
+// 	mongoose.connection.db.collectionNames(function (err, names) {
+//       // console.log(names); // [{ name: 'dbname.myCollection' }]
 		
-    });
+//     });
 
-  }
-});
+//   }
+// });
   
 
  
-var oneDay = 1;
+// var oneDay = 1;
 
-// set up our express application
+// // set up our express application
 
-console.log('static contents at: ' + __dirname + '\\client');
-app.use(express.static(__dirname + '/client', { maxAge: oneDay }));
-app.use(morgan('dev')); 											// log every request to the console
-app.use(bodyParser.json());
-app.use(methodOverride()); 											// simulate DELETE and PUT 
+// console.log('static contents at: ' + __dirname + '\\client');
+// app.use(express.static(__dirname + '/client', { maxAge: oneDay }));
+// app.use(morgan('dev')); 											// log every request to the console
+// app.use(bodyParser.json());
+// app.use(methodOverride()); 											// simulate DELETE and PUT 
  
  
-// Make our db accessible to our router
+// // Make our db accessible to our router
 
-app.use(function(req,res,next){
-    console.log('Time: %d', Date.now());
-	req.NotesModel = mongoose.model('notes', configDB.notes);
- 	req.UrlsModel = mongoose.model('urls', configDB.urls);
- 	req.OptionsModel = mongoose.model('options', configDB.options);
-	next();
-});
-
-
-console.log('routing...');
+// app.use(function(req,res,next){
+//     console.log('Time: %d', Date.now());
+// 	req.NotesModel = mongoose.model('notes', configDB.notes);
+//  	req.UrlsModel = mongoose.model('urls', configDB.urls);
+//  	req.OptionsModel = mongoose.model('options', configDB.options);
+// 	next();
+// });
 
 
+// console.log('routing...');
 
 
-// routes ======================================================================
-require('./app/routes.js')(app,express,request); // load our routes and pass in our app and fully configured passport
 
-// launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+
+// // routes ======================================================================
+// require('./app/routes.js')(app,express,request); // load our routes and pass in our app and fully configured passport
+
+// // launch ======================================================================
+// app.listen(port);
+// console.log('The magic happens on port ' + port);
