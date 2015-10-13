1d712d998f414d4427c2c9210116571980f97d2b
George <gnthackray@btinternet.com>
2015-10-13 00:40:49 +0000

    edges stuff

--- client/app/model/NodeManager.js
+++ client/app/model/NodeManager.js
@@ -16,7 +16,7 @@ var NodeManager = function (data,meta,options) {
     this.deleteNode =false;
     this.editNode =false;
     this.selectedNote;
-    this.nodeFactory;
+    
 };
 
 
@@ -159,7 +159,7 @@ NodeManager.prototype = {
         this.generations.push([]);
         this.generations[0] = [];
 
-        var nodeFactory = new Node();
+        var nodeFactory = new Node(this.generations);
 
         nodeFactory.CreateEmptyNode(true,false, function(node){
             this.AddData(0,false,node,function(){});
