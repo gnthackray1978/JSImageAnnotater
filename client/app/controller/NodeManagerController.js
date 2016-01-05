b2477f11f08e9d1ad756af8781701d6f8adf54e4
George <gnthackray@btinternet.com>
2016-01-05 11:29:30 +0000

    rectangular selection work

--- client/app/controller/NodeManagerController.js
+++ client/app/controller/NodeManagerController.js
@@ -13,7 +13,7 @@ var NodeManagerController = function (view, nodeDataManager, metadata,options,ch
     
     this.stateCache =0;
     
-    this._view.SelectNodeButton($.proxy(this.selectNodeAction, this));
+    
     this._view.DeleteNodeButton($.proxy(this.deleteNodeAction, this));
     
     this._view.DeleteSingleNodeButton($.proxy(this.deleteSingleNodeAction, this));
@@ -23,16 +23,7 @@ var NodeManagerController = function (view, nodeDataManager, metadata,options,ch
     this._view.MultiSelectNodeButton($.proxy(this.multiSelectNode, this));
     
     this._view.SaveNote($.proxy(this.saveAction, this));
-    
-    
-    
-  //  this._view.CanvasClick($.proxy(this.clickAction, this));
-   
-  //  this._view.CanvasDoubleClick($.proxy(this.doubleClickAction, this));
-   
-    //note operations
-    this._view.Add($.proxy(this.addButtonClicked, this));
-    
+ 
     this._view.Cancel($.proxy(this.cancelButtonClicked, this));
     
     
@@ -64,61 +55,50 @@ var NodeManagerController = function (view, nodeDataManager, metadata,options,ch
       //  that.updateState();
     });
     
-    this._channel.subscribe("singleselectchange", function(data, envelope) {
+    this._channel.subscribe("nodeselected", function(data, envelope) {
         
     });
     
-    this._channel.subscribe("singleClick", function(data, envelope) {
-        that.clickAction(data.value.x, data.value.y);
+    this._channel.subscribe("nodedeselected", function(data, envelope) {
+        //that.state =0;
     });
     
-    this._channel.subscribe("doubleClick", function(data, envelope) {
-        that.doubleClickAction(data.value.x, data.value.y);
+    // delete node
+    this._channel.subscribe("focusednode", function(data, envelope) {
+        //node contained with clicked pointer but wasnt selected.
+        //for deleting
+        that.selectedNote = data.node;
+      
+        that.state =6;
+        
+        that.updateState();
     });
     
-    /*
+    // add node
+    this._channel.subscribe("nullselection", function(data, envelope) {
+        //for adding
+        
+        if(that.state == 4) 
+        {
+            that.restoreCachedNode();
+            that.state =0;
+        }
+        
+        if(that.state == 1) that.state =5;
+        
+        that.updateState();
+    });
     
-    so what about when we need to update other models from this controller.
-    
-    PROPOSED
-    
-    ACTIONS
-    1. activateAction s1
-    2. s1 node clicked s2
-    3. s3 edit node s3
-    3. s4 cancel edit node s2
-    4. s2 s3 delete node(s) s1
-    5. add node s1
-    6. selection tool activated 
-    
-    STATES
-    0. inactive
-    1. node selectable
-    2. nodes selectable
-    
-    3. nodes selected
-    4. node selected
-    5. node editting
-    
-     
-    */
-    
-    /*
-    
-    CURRENT 
-    0. inactive
-    
-    1. free to edit mode
-    2. delete mode
-    
-    3. valid save state
-    
-    
-    4. node selected
-    5. node editting
-    */
-    
-    
+    //edit existing node
+    this._channel.subscribe("doubleClickSelectionChange", function(data, envelope) {
+        
+        that.selectedNote = data.node;
+      
+        that.state =4;
+        
+        that.updateState();
+    });
+
     this.state = 0;
     
     this.updateState();
@@ -138,22 +118,22 @@ NodeManagerController.prototype = {
                 
                 this._view.DisplayNeutralState();
                 this._view.ClearActiveTextArea();
-                this.nodeManager.DeSelectNodes(function(){
-                    that._channel.publish( "drawtree", null);
-                });
-                
                 
                 break;
             case 1: //FREE TO WRITE MODE
                 this._channel.publish( "lock", { value: true } );
                 this._view.DisplayAddState();
+                this._channel.publish( "activateStandardSelection", { value: true } );
               
                 break;
                 
             case 2: //FREE TO DELETE MODE
             
                 this._view.DisplayDeleteState();
+                this._channel.publish( "activateFocusedSelection", { value: true } );
+                
                 break;
+                
             case 3: //VALID TO SAVE
                 this._view.DisplaySaveState();
                 break;
@@ -175,17 +155,12 @@ NodeManagerController.prototype = {
                 });
                 that.selectedNote = undefined;
                 break;
-                
-                
-            case 7: //SELECTING
-                console.log('updateState: selecting');
-                this.selectNote();
-                break;
-                
             case 8: //FREE TO SELECT
                 console.log('updateState: free to select');
                 this._view.DisplaySelectionState();
-                break;      
+                this._channel.publish( "activateStandardSelection", { value: true } );
+                break;     
+                
             case 9: //multi selecting
                 console.log('updateState: multi selecting');
                 break;
@@ -195,16 +170,7 @@ NodeManagerController.prototype = {
         }
         
     },
-    
-    activateAction : function(){
-        this.state = 1;
-        this.updateState();
-    },
-    
-    activateAction : function(){
-        this.state = 0;
-        this.updateState();
-    },
+
     
     nodeTextChanged: function(e){
         //console.log('text changed: ' + e);
@@ -261,37 +227,6 @@ NodeManagerController.prototype = {
         });
     },
     
-    selectNodeAction: function(){
-        
-        if(this.state == 8 ) 
-            this.state =0;
-        else
-            this.state =8;
-        
-        this.updateState();
-    },
-    
-    selectNote: function(){
-        this.state = 8;
-        
-        if(this.selectedNote){
-            
-            if(this.selectedNote.Selected == undefined) {    
-                this.selectedNote.Selected = true;
-            }
-            else
-            {
-                this.selectedNote.Selected = !this.selectedNote.Selected;
-            }
-        }
-        else
-            console.log('selected note undefined');
-            
-            
-        this.updateState();
-    },
-    
-    
     deleteSingleNodeAction : function(){
         console.log('delete note'); 
     
@@ -319,83 +254,12 @@ NodeManagerController.prototype = {
         
         this.updateState();
     },
-    addButtonClicked:function(){
-        this.state =1;
-        this.updateState();
-    },
-    
     
     multiSelectNode : function(){
         this.state = 1;
         this.updateState();
     },
-    
-    
-    clickAction:function(x,y){
-        var that = this;
-     
-        if(this._mouseClickLocked) return;
-        
-        this.nodeManager.PointToNode(x,y, function(node){
-            that.selectedNote = node;
-            
-            if(that.selectedNote != node && that.selectedNote.Index != node.Index ){
-               that._channel.publish( "selectednodechanged", { value: that.selectedNote } ); 
-            }
-       
-            // add/edit node
-            if(that.state == 8 && that.selectedNote != undefined) that.state =7;
-            if(that.state == 1 && that.selectedNote == undefined) that.state =5;
-            
-            if(that.state == 2 && that.selectedNote != undefined) that.state =6;
-          
-            if(that.state == 4 && that.selectedNote == undefined) 
-            {
-                that.restoreCachedNode();
-                that.state =0;
-                
-            }
-            
-            if(that.state == 10 && that.selectedNote == undefined) 
-            {
-                that.state =0;
-            }
-            
-            if(that.state == 9) 
-            {
-                that.state = 10;
-            }
-            
-            
-            that.updateState();
-        });
-       
-    },
 
-    
-    doubleClickAction:function(x,y){
-        var that = this;
-     
-        if(this._mouseClickLocked) return;
-        
-        this.nodeManager.PointToNode(x,y, function(node){
-            that.selectedNote = node;
-            
-            if(that.selectedNote != node && that.selectedNote.Index != node.Index ){
-               that._channel.publish( "selectednodechanged", { value: that.selectedNote } ); 
-            }
-       
-            // add/edit node
-            if(that.state == 0 && that.selectedNote != undefined) that.state =4;
-            
-            that.updateState();
-        });
-       
-    },
-
-    
-   
-   
     cancelNodeButton:function(){
         this.state =0;
         this.updateState();
--- client/app/controller/RectangleSelectController.js
+++ client/app/controller/RectangleSelectController.js
@@ -1,7 +1,12 @@
 (function(exports){
 
-    var RectangleSelectController = function (view, channel, model) {
+    var RectangleSelectController = function (view, channel, model,nodeDataManager) {
+        this._isMultiSelecting =false;
         this._view = view;
+        this._state =0;
+        this._mouseClickLocked =false;
+        this._nodeManager = nodeDataManager;
+        this._selectedNode;
         this.isStarted = false;
         
         this._channel = channel;
@@ -11,6 +16,8 @@
         
         this.init();
         var that = this;
+        
+        this._view.SelectNodeButton($.proxy(this.selectNodeAction, this));
         
         this._channel.subscribe("selectionRectangleActivated", function(data, envelope) {
             //console.log('s_selectionRectangleActivated');
@@ -31,14 +38,43 @@
             //console.log('s_selectionMouseMove');
             that.qryCanvasMouseMove(data.value);
         });
+        
+        this._channel.subscribe("singleClick", function(data, envelope) {
+            that.clickAction(data.value.x, data.value.y);
+        });
     
+        this._channel.subscribe("doubleClick", function(data, envelope) {
+            that.doubleClickAction(data.value.x, data.value.y);
+        });
+        
+        this._channel.subscribe("mouseClickLock", function(data, envelope) {
+            that._mouseClickLocked =data.value;
+        });
+        
+        this._channel.subscribe("activateStandardSelection", function(data, envelope) {
+            that.selectNodeAction();
+        });
+        
+        this._channel.subscribe("activateFocusedSelection", function(data, envelope) {
+            that.deleteNodeAction();
+        });
+        
+        this._channel.subscribe("deactivateSelection", function(data, envelope) {
+            that._state =0;
+        });
+        
+        /*
+            selection cleared
+            selection changed
+            
+        */
     };
 
     RectangleSelectController.prototype = {
         init:function(){
             
         },
-    
+        
         qryCanvasMouseDown:function(evt){
             var that =this;
             
@@ -56,13 +92,6 @@
         
         qryCanvasMouseUp:function(evt){
             if (this.model !== null) {
-             
-            //     this._channel.publish( "lockmousemove", { value: '' } );
-    	       // this._channel.publish( "lockmouseup", { value: '' } );
-            //     this._channel.publish( "lockmousedown", { value: '' } );
-                
-                //this.model.CloseSelection();
-                
                 this.finishSelecting();
             }
         },
@@ -91,20 +120,17 @@
     	    	    that._channel.publish( "lockmousedown", { value: that._mouseLockKey} );
     	    	    that._view.DisplayRectangleSelection(true);
     	    	    that._channel.publish( "multiselectingstart", { value: this.model } );
+    	    	    that._isMultiSelecting =true;
         	    });
             }else
             {
-                //that._channel.publish( "lockmouseup", { value: '' } );
-                //that._channel.publish( "lockmousedown", { value: ''} );
-                //that._view.DisplayRectangleSelection(false);
-    	   
-    	        that.finishSelecting();
+                that.finishSelecting();
             }
         }, 
         
         finishSelecting : function(){
             
-            this.isStarted = false;
+            
             this._channel.publish( "lockmouseup", { value: '' } );
 	    	this._channel.publish( "lockmousedown", { value: ''} );
 	    	this._channel.publish( "lockmousemove", { value: '' } );
@@ -114,7 +140,106 @@
 	    	this.model.CloseSelection();
 	    	
 	    	this._channel.publish( "multiselectingend", { value: this.model } );
+	    	
+	    	this.isStarted = false;
+	    	
+	    	this._isMultiSelecting =false;
+        },
+
+        clickAction:function(x,y){
+            var that = this;
+         
+            if(this._mouseClickLocked) return;
+            
+            this._nodeManager.PointToNode(x,y, function(node){
+                
+                if(that._state ==0 ) return; // selections have been turned off.
+                
+                if(node == undefined && !that._isMultiSelecting){
+                    that.nodeManager.DeSelectNodes(function(){
+                        that._channel.publish( "drawtree", null);
+                    });
+                    
+                    that._channel.publish( "nullselection", { value: this.model } ); 
+                }
+                
+                if(node && that._state == 1){ // standard selection
+                    that._selectedNode = node;
+                    that.selectNode();
+                }
+                
+                if(node && that._state == 2){ // no selecting selection ie you point to a node return it so it can be deleted.
+                    that._selectedNode = node;
+                    that._channel.publish( "focusednode", { value: this.model } ); 
+                }
+            });
+       
+        },
+
+        doubleClickAction:function(x,y){
+            var that = this;
+         
+            if(this._mouseClickLocked) return;
+            
+            
+            
+            this._nodeManager.PointToNode(x,y, function(node){
+                
+                if(that._state ==0 ) return; // selections have been turned off.
+                
+                if(node)
+                    that._channel.publish( "doubleClickSelectionChange", { value: node } ); 
+            });
+           
+        },
+
+        selectNode: function(){
+             
+            if(this._selectedNode){        
+                if(this._selectedNode.Selected == undefined) {    
+                    this._selectedNode.Selected = true;
+                    this._channel.publish( "nodeselected", { value: this.model } ); 
+                }
+                else
+                {
+                    this._selectedNode.Selected = !this._selectedNode.Selected;
+                    
+                    if(this._selectedNode.Selected)
+                        this._channel.publish( "nodeselected", { value: this.model } ); 
+                    else
+                        this._channel.publish( "nodedeselected", { value: this.model } ); 
+                }
+            }
+            else
+                console.log('selected note undefined');
+                
+                
+            this._channel.publish( "drawtree", { value: this.model } );    
+        },
+        
+        
+        selectNodeAction: function(){ // users are clicking nodes and they are being selected/deselected
+            
+            if(this._state ==2 ) return;
+            
+            if(this._state == 1) 
+                this._state =0;
+            else
+                this._state =1;
+        },
+        
+        deleteNodeAction: function(){ // users are clicking nodes and they are being deleted
+            
+            if(this._state ==1 ) return;
+            
+            if(this._state == 2) 
+                this._state = 0;
+            else
+                this._state =2;
+            
         }
+
+
 
     };
 
