6a588deb36290bc5f486f7d0689e201668f0bd37
George <gnthackray@btinternet.com>
2016-01-24 17:25:45 +0000

    debug view stuff

--- client/app/views/DebugView.js
+++ client/app/views/DebugView.js
@@ -14,10 +14,21 @@ DebugView.prototype.Init= function (){
     var d = new Date();
     
     that.AddDebugRow(d.toLocaleTimeString(),'mouse','apples and pairs loads of info');
+    that.AddDebugRow(d.toLocaleTimeString(),'mouse','apples and pairs loads of info');
+    that.AddDebugRow(d.toLocaleTimeString(),'mouse','apples and pairs loads of info');
+    that.AddDebugRow(d.toLocaleTimeString(),'mouse','apples and pairs loads of info');
+    that.AddDebugRow(d.toLocaleTimeString(),'mouse','apples and pairs loads of info');
+    that.AddDebugRow(d.toLocaleTimeString(),'mouse','apples and pairs loads of info');
+    that.AddDebugRow(d.toLocaleTimeString(),'mouse','apples and pairs loads of info');
+    that.AddDebugRow(d.toLocaleTimeString(),'mouse','apples and pairs loads of info');
+    that.AddDebugRow(d.toLocaleTimeString(),'mouse','apples and pairs loads of info');
 };
 
 DebugView.prototype.AddDebugRow= function (time, name, description){
     
+    var row = time + ' - ' + name + ' - ' + description;
+    
+    $('#debugoutput').append( "<p>"+ row +"</p>" );
      
 }
 
