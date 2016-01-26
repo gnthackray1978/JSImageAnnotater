c356b532ac215355cc9e5b44f04e081f7097b01b
George <gnthackray@btinternet.com>
2016-01-26 13:52:56 +0000

    meta data refactor

--- client/app/controller/MetaController.js
+++ client/app/controller/MetaController.js
@@ -38,7 +38,7 @@ var MetaController = function (model,channel, nodeManager) {
         that.model.Save(function(metaData){
             that.selectedNode.metaData  = metaData;
             
-            that.nodeManager.WriteToDB(that.selectedNode, function(){
+            that._nodeManager.WriteToDB(that.selectedNode, function(){
                 that._shout('MetaSaveButtonState','WriteToDB finished');
             });
             that._channel.publish( "MetaDataRefreshed", { value: that.selectedNode.metaData } );
